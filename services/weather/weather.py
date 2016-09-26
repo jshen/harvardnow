6506aa1e3a49a05387b86873fcc72238cb80605b
Eric Timmerman <estimmerman@gmail.com>
2016-09-25 20:39:32 -0400

    trying again to fix weather

--- services/weather/weather.py
+++ services/weather/weather.py
@@ -17,12 +17,12 @@ def getWeatherData(input):
     try:
         card = soup.find(id='ires').find_all(class_='g')[0]
 
-        label = card.h3.text + '\n' if card.h3.text else ''
+        label = card.h3.text + '\n' if card.h3 is not None else ''
 
-        overview = card.img.attrs['title'] + '\n' if card.img.attrs else ''
-        tempInFarenheit = 'Temp: ' + card.find_all(class_='wob_t')[0].text.encode('unicode-escape').replace(r'\xb0','') + '\n' if card.find_all(class_='wob_t')[0] else ''
-        humidity = card.find_all(text=re.compile('Humidity'))[0] + '\n' if card.find_all(text=re.compile('Humidity'))[0] else ''
-        wind = card.find_all(text=re.compile('Wind'))[0].parent.text if card.find_all(text=re.compile('Wind'))[0] else ''
+        overview = card.img.attrs['title'] + '\n' if card.img is not None && card.img.attrs is not None else ''
+        tempInFarenheit = 'Temp: ' + card.find_all(class_='wob_t')[0].text.encode('unicode-escape').replace(r'\xb0','') + '\n' if len(card.find_all(class_='wob_t')) > 0 else ''
+        humidity = card.find_all(text=re.compile('Humidity'))[0] + '\n' if len(card.find_all(text=re.compile('Humidity'))) > 0 else ''
+        wind = card.find_all(text=re.compile('Wind'))[0].parent.text if len(card.find_all(text=re.compile('Wind'))) > 0 else ''
 
         body = label
         body += overview
