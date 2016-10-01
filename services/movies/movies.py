c510883ec34ccd93fd165441821c29377bf378a4
Jake Steeves <jsteeves@college.harvard.edu>
2016-09-30 21:54:20 -0400

    started working on a week checker

--- services/movies/movies.py
+++ services/movies/movies.py
@@ -2,13 +2,50 @@ import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-def getMovies(cmd):
-    month = cmd.lower()
-    year = str(datetime.now().year)[-2::]
+def parseDay(day_info, month):
+    shows = ""
+    try:
+        day_title = month + " " + day_info[0].getText() + ":\n"
+        showtime = len(day_info[1].getText())
+    except:
+        showtime = 0
+    if showtime > 1:
+        shows += day_title 
+        show_info = "  "
+        day_shows = day_info[1:]
+        for index, show in enumerate(day_shows):
+            show_info += show.getText()                
+            if show["class"][0] == "calfilm":
+                shows += show_info + "\n"
+                try:
+                    next_el = day_shows[index + 1]
+                    if next_el["class"][0] == "calnote":
+                        shows += "    *" + next_el.getText() + "\n"
+                except:
+                    pass
+                show_info = "  "
+        show_info += "\n"
+    return shows
+
+def getWeek():
+    now = datetime.now()
+    day = now.day
+    month = now.month
+    year = str(now.year)[-2::]
     url = "http://hcl.harvard.edu/hfa/calendar/{0}{1}.html".format(month, year)
     website = urllib2.urlopen(url)
-    soup = BeautifulSoup(website.read(), 'html.parser')
+    soup = BeautifulSoup(website.read())
+    print day, month, year
+    print soup.find(text=str(day)).parent.parent
 
+def getMonth(cmd):
+    cmd = cmd.lower()
+    month = cmd[0].upper() + cmd[1:]
+    year = str(datetime.now().year)[-2::]
+    url = "http://hcl.harvard.edu/hfa/calendar/{0}{1}.html".format(cmd, year)
+    website = urllib2.urlopen(url)
+    soup = BeautifulSoup(website.read())
+    
     shows = ""
     weeks = soup.find(id="calendar").find_all("tr")
     for week in weeks:
@@ -16,22 +53,7 @@ def getMovies(cmd):
         for day in days:
             day_info = day.find_all("p")
             # ignore empty days
-            if len(day_info) > 2:
-                shows += cmd + " " + day_info[0].getText() + ":\n"
-                show_info = "  "
-                day_shows = day_info[1:]
-                for index, show in enumerate(day_shows):
-                    show_info += show.getText()                
-                    if show["class"][0] == "calfilm":
-                        shows += show_info + "\n"
-                        try:
-                            next_el = day_shows[index + 1]
-                            if next_el["class"][0] == "calnote":
-                                shows += "    *" + next_el.getText() + "\n"
-                        except:
-                            pass
-                        show_info = "  "
-                show_info += "\n"
+            shows += parseDay(day_info, month)
             
     print shows
 
