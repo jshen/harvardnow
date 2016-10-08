eecfca3301b9a6ef3b0b9b8d211f02607d11027e
Tovly Deutsch <tovlydeutsch@gmail.com>
2016-10-07 22:17:30 -0400

    Addded comments and cleaned up

--- services/MBTA/MBTA.py
+++ services/MBTA/MBTA.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 def getMBTAData(cmd):
     overall_body = []
 
+    # iterate through all station requests
     for pg in cmd['pg']:
         url = 'http://www.mbtainfo.com/'
         url += pg
@@ -18,14 +19,15 @@ def getMBTAData(cmd):
         soup = BeautifulSoup(website.read(), 'html.parser')
 
         try:
-            #print(soup)
+            # get elements from a.minor until div.ads
             times = soup.find("a", class_="minor").find_next_siblings()
+            # delete all elements after last ul
             for i, x in enumerate(reversed(times)):
                 if x.name == 'ul':
                     times = times[:-i]
                     break
 
-
+            # define generator function for generating the proper string from each html element
             def gen_body():
                 for element in times:
                     for string in element.strings:
@@ -33,6 +35,7 @@ def getMBTAData(cmd):
 
             body = gen_body()
 
+            # add new lines before any header lines
             for b in filter(lambda x: len(x) > 1, body):
                 if not (unicode(b)[0].isdigit() or b.startswith("Arriving")):
                     overall_body.append("")
@@ -40,8 +43,10 @@ def getMBTAData(cmd):
 
         except Exception, e:
             print str(e)
-            return "Could not find T data. Are you sure you gave a proper line and station name?"
+            return "Could not find T data. Are you sure you gave a proper line and station name? Additionally, "
+            "this service only works for red, blue, orange, and green lines."
 
+    # return all the train information stings joined as one
     return "\n".join(overall_body[1:])
 
 ############################
@@ -52,11 +57,12 @@ def makeSpecial():
     s = 'To get the shuttle for a particular line, use the format \'MBTA line station\'.'
     return s
 
-## return proper format to use for getting weather
+# return proper format to use for getting train information
 special = makeSpecial()
 
 def eval(cmd):
     return getMBTAData(cmd)
 
+# pass mock values if running from terminal
 if __name__ == "__main__":
-    print(getMBTAData({"pg": ["red/RHAR", "green/haecl"]}))
+    print(getMBTAData({"pg": ["red/RHAR", "green/haecl", "blue/BWON"]}))
