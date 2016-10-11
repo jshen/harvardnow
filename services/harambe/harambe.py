e0898513eeb149a6ab7e8505e019a4d720df1e44
Jack Zhang <15jzhang@gmail.com>
2016-10-11 01:19:04 -0400

    Implement harambe service
    Scrapes and returns the first link from the harambe subreddit

--- services/harambe/harambe.py
+++ services/harambe/harambe.py
@@ -5,8 +5,47 @@ from bs4 import BeautifulSoup
 ##    Harambe Function     ##
 #############################
 
+# Scrapes the current top link from the harambe subreddit
+# https://www.reddit.com/r/harambe
+def getHarambePost():
+    base_url = 'https://www.reddit.com'
+    subreddit_url = base_url + '/r/harambe'
+
+    hdr = {'User-Agent': 'Chrome'}
+    req = urllib2.Request(subreddit_url, headers=hdr)
+    website = urllib2.urlopen(req)
+    soup = BeautifulSoup(website.read(), 'html.parser')
+
+    try:
+        top_post = soup.find_all('a', class_='title')[0]
+        link = top_post.get('href')
+        if link[:4] != 'http':
+            link = base_url + link
+
+    except Exception, e:
+        print str(e)
+        return "Could not scrape harambe subreddit :("
+
+    return link
+
+def makeEvalString():
+    gorilla = \
+"""\
+      ."`".
+  .-./ _=_ \.-.
+ {  (,(oYo),) }}
+ {{ |   "   |} }
+ { { \(---)/  }}
+ {{  }'-=-'{ } }
+ { { }._:_.{  }}
+ {{  } -:- { } }
+ {_{ }`===`{  _}
+((((\)     (/))))\
+"""
+    return gorilla + '\n\n' + getHarambePost()
+
 def makeSpecial():
-    return 'DIX OUT'
+    return 'Get the top post from /r/harambe.'
 
 ############################
 ##       Top-Level        ##
@@ -14,5 +53,5 @@ def makeSpecial():
 
 special = makeSpecial()
 
-def eval(cmd):
-    return special
+def eval():
+    return makeEvalString()
