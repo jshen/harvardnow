eab1a2eae5ffb208039b6d57b87f4bba32b7671a
Jack Zhang <15jzhang@gmail.com>
2016-10-11 00:12:34 -0400

    Setup harambe service

---
+++ services/harambe/harambe.py
@@ -0,0 +1,18 @@
+import urllib2, urllib
+from bs4 import BeautifulSoup
+
+#############################
+##    Harambe Function     ##
+#############################
+
+def makeSpecial():
+    return 'DIX OUT'
+
+############################
+##       Top-Level        ##
+############################
+
+special = makeSpecial()
+
+def eval(cmd):
+    return special
