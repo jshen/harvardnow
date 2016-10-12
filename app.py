3ec913d36d6b0bb046492265d36faad8c0915979
Theodore Liu <theodore_liu@college.harvard.edu>
2016-10-12 19:38:48 -0400

    added "day in history" functionality

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ def eval(cmd, input=None):
         return shuttle.eval(cmd['args'])
     elif cmd['service'] == 'W': ## Weather
         return weather.eval(input)
+    elif cmd['service'] == 'H': ## History
+        return history.eval(input)
     else:
         return "ERROR 42: service not recognized"
 
@@ -32,6 +34,8 @@ def special(incoming):
     elif incoming.upper() == "LAUNDRY":
         body = laundry.special
     elif incoming.upper() == "WEATHER":
+        body = weather.special
+    elif incoming.upper() == "HISTORY":
         body = weather.special
     elif incoming.upper() == "DEMO":
         ## welcome/instructions
---
+++ services/history/history.py
@@ -0,0 +1,21 @@
+import urllib2, urllib
+import re
+from bs4 import BeautifulSoup
+
+def getHistory(input):
+	url = "http://www.history.com/this-day-in-history"
+	page = urllib2.urlopen(url)
+	soup = BeautifulSoup(page.read(), 'lxml')
+
+	headers = soup.find_all('h2')
+
+	return "Today in history: " + headers[1].text
+
+def makeSpecial():
+	s = "To get what happened in history today, please enter \'history\'"
+	return s
+
+special = makeSpecial()
+
+def eval(input):
+	return getHistory(input)
