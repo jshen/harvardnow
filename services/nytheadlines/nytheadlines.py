9cc2067f99f5449c3d1f29a2136b23abd3f90c2a
AustT1996 <tripp.aust@gmail.com>
2016-10-11 22:26:46 -0400

    Initialized my two python service files

---
+++ services/nytheadlines/nytheadlines.py
@@ -0,0 +1,17 @@
+import urllib2, urllib
+
+
+
+# Mandatory functions/things
+
+special = "TODO"  # TODO
+
+def eval(cmd):
+    pass  # TODO!!
+
+
+
+
+# Testing:
+if __name__ == "__main__":
+    print("Todo!")
