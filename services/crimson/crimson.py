f421c68e005e7bfbc83dcf8f632d89c9b3a8db12
James Lennon <jameslennon321@gmail.com>
2016-10-13 19:17:24 -0400

    Removed old search code, updated special

--- services/crimson/crimson.py
+++ services/crimson/crimson.py
@@ -2,13 +2,10 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-from api import API
-
-SEARCH_URL_OLD = "https://www.thecrimson.com/search/?cx=013815813102981840311%3Aaw6l9tjs1a0&cof=FORID%3A10&ie=UTF-8&q="
-SEARCH_URL = "https://www.google.com/search?hl=en&as_q=REPLACE&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=&cr=&as_qdr=all&as_sitesearch=thecrimson.com&as_occt=any&safe=images&as_filetype=&as_rights="
 HOME_URL = "https://www.thecrimson.com/"
+
 # Temporary special string 
-special = "In order to receive a list of links to the top 5 crimson articles returned by searching the crimson for a string, use the command \'crimson [search]\'."
+special = "Enter 'crimson' to get the top 5 articles. 📖"
 
 # Get top articles
 # Returns a dictionary mapping article name to link
@@ -30,40 +27,15 @@ def getTopArticles():
 
 	return topArticles
 
-# One interesting thing is that when we run this search on the url, it sorts them by relevance.
-# There's a selector we might want to think about changing to sort by date
-#incomplete
-def searchCrimson(search):
-	# replacing whitespace in their search with a plus sign
-	search = search.replace(" ", "+")
-	url = SEARCH_URL
-	url.replace("REPLACE", search)
-	print(url)
-	website = urllib2.urlopen(url)
-	soup = BeautifulSoup(website.read(), 'html.parser')
-	print(soup.prettify())
-	resultTest = soup.findAll("div")
-	results = soup.findAll("div", class_="gsc-webResult gsc-result")
-	for i in resultTest:
-		i.prettify()
-
-	print results
-
-	for i in results: i.prettify()
-	# print(soup.prettify())
-
-
 def eval(cmd):
+	# TODO: parse cmd and return top articles string
 	pass
 
-# searchCrimson("HUDS")
 d = getTopArticles()
-# print(d)
 
 print "\n\n".join(map(lambda e: "{}: {}".format(e[0], e[1]), d))
 
-#for i in getTopArticles():
-#	print(i.prettify())
+
 
 
 
