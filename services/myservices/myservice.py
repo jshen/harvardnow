c2d10bd242c67a6e4f3d56867e14828e7161b7ea
jkim25 <j_kim@college.harvard.edu>
2017-03-16 19:50:25 -0400

    Update myservice.py

--- services/myservices/myservice.py
+++ services/myservices/myservice.py
@@ -1,1 +1,21 @@
+def eval(cmd, input=None):
+    s = ""
+    if cmd['service'] == 'M': ## Music
+        return laundry.eval(cmd['args'])
+    else:
+        return "ERROR 42: service not recognized"
 
+## list of services that need the user's input to work, not a command
+def needsInput(cmd):
+    return cmd['service'] in ['W']
+
+def special(incoming):
+    body = ''
+    if incoming.upper() == "Music" :
+        body = Music.special
+    elif incoming.upper() == "DEMO":
+      
+        ## welcome/instructions
+        body = 'Thanks for using Harvard Now!\n'
+        body += 'Text Music to view the Billboard Top 20 songs of the week\n'
+    return body
