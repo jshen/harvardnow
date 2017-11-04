1c83d8337da5eb28c83946aabe926d75c7fea699
FacistFuhrer <mikebao1999@gmail.com>
2017-11-03 20:53:59 -0400

    I win

---
+++ services/stock/stock.py
@@ -0,0 +1,54 @@
+import urllib2, urllib
+import re
+from bs4 import BeautifulSoup
+
+#############################
+##    Weather Function     ##
+#############################
+
+def getWeatherData(input):
+    url = 'https://finance.google.com/finance?q='
+    url += input
+    hdr = {'User-Agent': 'Chrome'}
+    req = urllib2.Request(url,headers=hdr)
+    website = urllib2.urlopen(req)
+    soup = BeautifulSoup(website.read(), 'html.parser')
+
+    try:
+        card = soup.find("div", {"id":"price-panel"}).find("span").find("span").string
+
+        card = '$' + card
+
+        return card
+
+        #label = card + '\n' if card.h3 is not None else ''
+
+        # overview = card.img.attrs['title'] + '\n' if card.img is not None and card.img.has_attr('title') else ''
+        # tempInFarenheit = 'Temp: ' + card.find_all(class_='wob_t')[0].text.encode('unicode-escape').replace(r'\xb0','') + '\n' if len(card.find_all(class_='wob_t')) > 0 else ''
+        # humidity = card.find_all(text=re.compile('Humidity'))[0] + '\n' if len(card.find_all(text=re.compile('Humidity'))) > 0 else ''
+        # wind = card.find_all(text=re.compile('Wind'))[0].parent.text if len(card.find_all(text=re.compile('Wind'))) > 0 else ''
+
+        # body = label
+        # body += overview
+        # body += tempInFarenheit
+        # body += humidity
+        # body += wind
+     except Exception, e:
+         print str(e)
+         return "Could not find stock data. Are you sure you gave a proper stock symbol?"
+
+    #return body
+
+############################
+##       Top-Level        ##
+############################
+
+def makeSpecial():
+    s = 'To get the stock, type in the stock symbol.'
+    return s
+
+## return proper format to use for getting weather
+special = makeSpecial()
+
+def eval(input):
+    return getWeatherData(input)
